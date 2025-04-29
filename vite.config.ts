a1a0dc78e46bf73b7f3a3f4c953fc442e1eae385
shizeying <w741069229@163.com>
2025-04-29 08:27:22 +0800

    Commit from Bolt to GitHub

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://api-frp.ipv4.name',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
